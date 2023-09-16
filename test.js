55023ca4834e5a3beb33271c34d65358031d9c0c
kjsorolcl <55680501+kjsorolcl@users.noreply.github.com>
2023-09-16 11:52:25 +0900

    Update test.js

--- test.js
+++ test.js
@@ -13,3 +13,4 @@ swfobject.embedSWF = function(url, cont, width, height){
 }
 
 swfobject.embedSWF('https://kjsorolcl.github.io/%E1%84%92%E1%85%A6%E1%86%AF%E1%84%85%E1%85%A9%E1%84%8F%E1%85%B5%E1%84%90%E1%85%B5-%E1%84%90%E1%85%B3%E1%86%AF%E1%84%85%E1%85%B5%E1%86%AB%E1%84%80%E1%85%B3%E1%84%85%E1%85%B5%E1%86%B7%E1%84%8E%E1%85%A1%E1%86%BD%E1%84%80%E1%85%B5.swf', 'ruffle', 700, 600);
+console.log('load!');
